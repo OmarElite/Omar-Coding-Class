d5311d6047169695a9f23d6e4b0f815b7d39352c
OmarElite <omar.elhaitote@gmail.com>
2025-05-17 14:35:13 +0100

    Lesson 57

--- Lesson 57/RealTimeFaceTrackingAndCountingPeople.py
+++ Lesson 57/RealTimeFaceTrackingAndCountingPeople.py
@@ -13,15 +13,15 @@ if not cap.isOpened():
 
 while True:
 	#capture frame by frame
-	ret, frame = cap.read()
+    ret, frame = cap.read()
 
-	# if frame is read correctly, ret will be true
-	if not ret:
-		print("Error: failed to capture image")
-		break
-
-	# convert frame to grayscale (face detection works better on grayscale)
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # if frame is read correctly, ret will be true
+    if not ret:
+            print("Error: failed to capture image")
+            break
+        
+    # convert frame to grayscale (face detection works better on grayscale)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # detect face in the grayscale image
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
@@ -39,7 +39,7 @@ while True:
 
     # break the loops when 'q' is pressed
     if cv2.waitKey(1) & 0xFF == ord("q"):
-	    break
+        break
 
 # release the capture and close any open windows
 cap.release()
